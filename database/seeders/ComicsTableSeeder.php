d39d600a9a3076083d96f65431e6998d41e6302f
ludoff <ludovicofiore25@gmail.com>
2024-09-16 16:08:29 +0200

    feat:add table and seeder to populate via file config

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Comic;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // variabile per dati da file config
+        $comics= config('comics');
+
+        foreach ($comics as $comic) {
+            $new_comic= new Comic();
+            $new_comic->title = $comic['title'];
+            $new_comic->description = $comic['description'];
+            $new_comic->thumb = $comic['thumb'];
+            $new_comic->price = $comic['price'];
+            $new_comic->series = $comic['series'];
+            $new_comic->sale_date = $comic['sale_date'];
+            $new_comic->type = $comic['type'];
+            $new_comic->save();
+        }
+    }
+}
