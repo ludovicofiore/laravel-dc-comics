dda924376d2cbd50d398a733fd6af140d20453f1
ludoff <ludovicofiore25@gmail.com>
2024-09-16 18:11:11 +0200

    feat: add fill property

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -33,14 +33,18 @@ class ComicsController extends Controller
         $data= $request->all();
 
         $new_comic= new Comic();
-        $new_comic->title = $data['title'];
-        $new_comic->thumb = $data['thumb'];
-        $new_comic->description = $data['description'];
-        $new_comic->price = $data['price'];
-        $new_comic->series = $data['series'];
-        $new_comic->sale_date = $data['sale_date'];
-        $new_comic->type = $data['type'];
+        // $new_comic->title = $data['title'];
+        // $new_comic->thumb = $data['thumb'];
+        // $new_comic->description = $data['description'];
+        // $new_comic->price = $data['price'];
+        // $new_comic->series = $data['series'];
+        // $new_comic->sale_date = $data['sale_date'];
+        // $new_comic->type = $data['type'];
+
+
+        $new_comic->fill($data);
         $new_comic->save();
+
 
         return redirect()->route('comics.show', $new_comic->id);
 
