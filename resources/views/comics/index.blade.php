8c9eb89d2ba1ad12c6aad4a15affb99cc24df07f
ludoff <ludovicofiore25@gmail.com>
2024-09-17 17:25:36 +0200

    fix: layout fixes

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -7,7 +7,7 @@
         </div>
     @endif
 
-    <div class="container my-5 d-flex flex-wrap">
+    <div class="container my-5 d-flex flex-wrap justify-content-between">
 
 
         @foreach ($comics as $comic)
@@ -16,18 +16,25 @@
                 <div class="card-body">
                     <h3 class="card-title">{{ $comic->title }}</h3>
                     <h5>{{ $comic->price }}</h5>
-                    <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary">Dettagli</a>
-                    <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning">Modifica</a>
 
-                    {{-- form per pulsante delete --}}
-                    <form action="{{ route('comics.destroy', $comic) }}" method="post"
-                        onsubmit="return confirm('Sei sicuro di voler eliminare {{ $comic->title }}?')">
+                    <div class="d-flex justify-content-around">
 
-                        @csrf
-                        @method('DELETE')
+                        <a href="{{ route('comics.show', $comic->id) }}" class="btn btn-primary" title="Dettagli"><i
+                                class="fa-solid fa-eye"></i></a>
+                        <a href="{{ route('comics.edit', $comic->id) }}" class="btn btn-warning" title="Modifica"><i
+                                class="fa-solid fa-pencil"></i></a>
 
-                        <button class="btn btn-danger" type="submit">Elimina</button>
-                    </form>
+                        {{-- form per pulsante delete --}}
+                        <form action="{{ route('comics.destroy', $comic) }}" method="post"
+                            onsubmit="return confirm('Sei sicuro di voler eliminare {{ $comic->title }}?')">
+
+                            @csrf
+                            @method('DELETE')
+
+                            <button class="btn btn-danger" type="submit" title="Elimina"><i
+                                    class="fa-solid fa-trash"></i></button>
+                        </form>
+                    </div>
                 </div>
             </div>
         @endforeach
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,9 @@
 
     <title>Comics | @yield('titlePage')</title>
 
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css'
+        integrity='sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg=='
+        crossorigin='anonymous' />
     {{-- Includiamo gli assets con la direttiva @vite --}}
     @vite('resources/js/app.js')
 </head>
